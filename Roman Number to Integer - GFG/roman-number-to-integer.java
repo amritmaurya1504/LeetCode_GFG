3213ec8a9e8f1a4b1dcfa6c8a6a84f71335ddb6b
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2022-10-20 15:34:27 +0530

    Added solution - LeetHub

---
+++ Roman Number to Integer - GFG/roman-number-to-integer.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String roman = br.readLine().trim();
+            Solution ob = new Solution();
+            System.out.println(ob.romanToDecimal(roman));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Finds decimal value of a given roman numeral
+    public int romanToDecimal(String str) {
+        // code here
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int ans = 0;
+        int size = str.length();
+        for(int i=0; i<size-1; i++){
+            if(map.get(str.charAt(i)) < map.get(str.charAt(i+1)))
+                ans -= map.get(str.charAt(i));
+            else
+                ans += map.get(str.charAt(i));
+        }
+
+        ans += map.get(str.charAt(size-1));
+        return ans;
+    }
+}
