866333f9783c2c663957b4dd914f2344acc34683
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2023-02-21 20:59:52 +0530

    Added solution - LeetHub

---
+++ Isomorphic Strings - GFG/isomorphic-strings.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.*;
+
+
+public class Driver {
+    
+    public static void main(String[] args)throws IOException {
+       
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while(tc-->0)
+        {
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+            
+            Solution obj = new Solution();
+            
+            boolean a = obj.areIsomorphic(s1,s2);
+            if(a)
+            System.out.println(1);
+            else
+            System.out.println(0);
+        }
+    }
+    
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to check if two strings are isomorphic.
+    public static boolean areIsomorphic(String s,String t)
+    {
+         if(s.length() != t.length()) return false;
+
+        HashMap<Character, Character> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(map.containsKey(c1)){
+                if(map.get(c1) != c2) return false;
+            }else if(map.containsValue(c2)){
+                return false;
+            }else{
+                map.put(c1,c2);
+            }
+        }
+
+        return true;
+        
+    }
+}
