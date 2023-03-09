c0aa513a6c6d0673d2e77d967c3f0277996614ec
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2023-03-09 21:50:34 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Compute obj = new Compute();
+            long answer[] = obj.printFirstNegativeInteger(a, n, k);
+            int sz = answer.length;
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<sz;i++)
+                output.append(answer[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long[] printFirstNegativeInteger(long A[], int N, int K)
+    {
+        Queue<Long> q = new LinkedList<>();
+        int i = 0, j = 0;
+        long[] ans = new long[N-K+1];
+        int a = 0;
+        while(j < N){
+            // doing some calculations
+            if(A[j] < 0){
+                q.offer(A[j]);
+            }
+            
+            if(j - i + 1 < K) j++;
+            else if(j - i + 1 == K){
+                // find answer from calculation
+                if(q.size() == 0) ans[a++] = 0;
+                else{
+                    ans[a++] = q.peek();
+                    if(q.peek() == A[i]){
+                        q.poll();
+                    }
+                }
+                i++;
+                j++;
+            }
+            
+        }
+        return ans;
+    }
+}
