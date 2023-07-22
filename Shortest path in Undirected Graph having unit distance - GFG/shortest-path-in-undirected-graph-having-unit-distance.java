c2fe49d4a4bf34b22ac3739fc9e82770a158243e
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2023-07-22 23:36:03 +0530

    Added solution - LeetHub

---
+++ Shortest path in Undirected Graph having unit distance - GFG/shortest-path-in-undirected-graph-having-unit-distance.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    
+    public int[] shortestPath(int[][] edges,int n,int m ,int src) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        // creating adjacency List
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        
+        for(int i = 0; i < m; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        
+        // Now Normal BFS Call with slight modification
+        int[] dist = new int[n];
+        for(int i = 0; i < n; i++) dist[i] = (int) 1e9;
+        Queue<Integer> q = new LinkedList<>();
+        dist[src] = 0;
+        q.add(src);
+        
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            
+            for(int it : adj.get(node)){
+                if(dist[node] + 1 < dist[it]){
+                    dist[it] = 1 + dist[node];
+                    q.add(it);
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            if(dist[i] == 1e9) dist[i] = -1;
+        }
+        
+        return dist;
+    }
+}
+
+
+
+
+
