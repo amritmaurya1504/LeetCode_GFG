12d02b2938b0e0d5376bcc540d5d7f279fac3354
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2023-10-06 00:11:59 +0530

    Added solution - LeetHub

---
+++ Reverse alternate nodes in Link List - GFG/reverse-alternate-nodes-in-link-list.java
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Rearr
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t  =sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node temp = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                temp.next = new Node(a);
+                temp = temp.next;
+            }
+            
+            Solution ob = new Solution();
+            ob.rearrange(head);
+            printLast(head);
+            System.out.println();
+        }
+    }
+    
+    public static void printLast(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}*/
+class Solution
+{
+    public static void rearrange(Node odd)
+    {
+        // add your code here
+        Node od=odd,evenHead=odd.next,even=evenHead;
+        
+        while(even!=null && even.next!=null){
+           od.next=even.next;
+           od=od.next;
+           even.next=od.next;
+           even=even.next;
+       }
+        
+        evenHead=reverse(evenHead);
+       od.next=evenHead;
+        
+        
+    }
+    public static Node reverse(Node head){
+        Node curr =  head, prev=  null, next = null;
+        while(curr!=null){
+            next  =  curr.next;
+            curr.next =  prev ;
+            prev =  curr;
+            curr  =  next;
+        }
+        return prev;
+    } 
+}
